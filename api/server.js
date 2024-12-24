688e5e5848c875d9f7ebdcb3a32f54b7751a0d85
nareshtap <naresh.tap@gmail.com>
2024-12-24 12:50:26 +0530

    modified vercel and server.js

--- api/server.js
+++ api/server.js
@@ -1,13 +1,12 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("./db.json");
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(3000, () => {
-  console.log("JSON Server is running");
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
 });
-
-module.exports = server;
