4941fb10c691d6417f957f4a994eaa6a825e7ab6
nareshtap <naresh.tap@gmail.com>
2024-12-24 12:14:55 +0530

    modified structure for deployment

--- json-server-api/api/server.js
+++ json-server-api/api/server.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router("./db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
@@ -9,3 +9,5 @@ server.use(router);
 server.listen(3000, () => {
   console.log("JSON Server is running");
 });
+
+module.exports = server;
